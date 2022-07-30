8322dbf0125de65b549aa2da6784cabbac92b1cf
kimsuha <suha970829@naver.com>
2022-07-30 20:59:02 +0900

    DFS순회 재귀로 다시 풀어보기

--- 타겟_넘버.js
+++ 타겟_넘버.js
@@ -1,18 +1,24 @@
 function solution(numbers, target) {
   var answer = 0;
 
-  //왼쪽 -1  오른쪽 +1
-  //gragh:깊이 sum:합
+  //DFS 재귀로 풀기
+
+  //graph : 현재위치(0 ~ numbers.length)
+  //sum : 합
   function dfs(graph, sum) {
     if (graph === numbers.length) {
+      // 탈출조건
       if (sum === target) {
-        answer += 1;
+        //합이 target과 같아지면, answer값 1개 증가.
+        answer++;
       }
       return;
     }
-    dfs(graph + 1, sum - numbers[graph]);
-    dfs(graph + 1, sum + numbers[graph]);
+    dfs(graph + 1, sum + numbers[graph]); //sum에 numbers[graph]값 더하기
+    dfs(graph + 1, sum - numbers[graph]); //sum에 numbers[graph]값 빼기
   }
-  dfs(0, 0);
+
+  dfs(0, 0); //graph와 sum을 0부터 시작
+
   return answer;
 }
